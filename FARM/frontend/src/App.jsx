5d13162d823abb035cba8f300b0354edb19d349c
sree-r-one <sreeraj_ec@hotmail.com>
2024-08-25 23:40:14 +0800

    dev for frontend completed

--- FARM/frontend/src/App.jsx
+++ FARM/frontend/src/App.jsx
@@ -1,14 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  {
+    console.log(import.meta.env.VITE_API_KEY);
+  }
 
   return (
     <>
       <div>
+        <h1>{import.meta.env.VITE_NODE_ENV}</h1>
+        <h1>{import.meta.env.VITE_API_KEY}</h1>
+
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -29,7 +36,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- FARM/frontend/vite.config.js
+++ FARM/frontend/vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: "/", // Adjust if you're serving from a subdirectory
   plugins: [react()],
-})
+  preview: {
+    port: 3000,
+    strictPort: true,
+  },
+  server: {
+    port: 3000,
+    strictPort: true,
+    host: "0.0.0.0", // This is correct for Docker
+    origin: "http://localhost:3000", // Use localhost or actual host IP
+  },
+});
